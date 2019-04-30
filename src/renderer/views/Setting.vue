50d8d490166c2eac0f7928556238283cbca7893f
leegeunhyeok <lghlove0509@naver.com>
2019-04-30 23:23:23 +0900

    UPDATE: Add background color and styles

--- src/renderer/views/Setting.vue
+++ src/renderer/views/Setting.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="setting">
     Setting
   </div>
 </template>
@@ -11,5 +11,18 @@ export default {
 </script>
 
 <style>
-
+#setting {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  padding: .5rem;
+  border-radius: .5rem;
+  background-color: #fff;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, .3);
+  box-sizing: border-box;
+  text-align: center;
+}
 </style>
