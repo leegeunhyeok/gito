1f220a0c82e748f1b6c88bd5fb10c17496630523
leegeunhyeok <lghlove0509@naver.com>
2019-04-30 17:42:28 +0900

    ADD: Loading.vue

---
+++ src/renderer/components/Loading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="loading"></div>
+</template>
+
+<script>
+export default {
+  name: 'loading-view'
+}
+</script>
+
+<style>
+#loading {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  border-radius: .5rem;
+  z-index: 9998;
+}
+</style>
