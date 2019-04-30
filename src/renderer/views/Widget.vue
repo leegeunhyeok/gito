e314f0391177e66670c229cb47284b8eaa2f9690
leegeunhyeok <lghlove0509@naver.com>
2019-04-30 18:27:52 +0900

    UPDATE: Rendering graph data & add styles (temp)

--- src/renderer/views/Widget.vue
+++ src/renderer/views/Widget.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="widget">
     <Loading v-if="loading"/>
+    <div class="graph-wrap">
+      <div class="week" v-for="(week, i) of history"
+        :key="i"
+      >
+        <span class="day" v-for="(day, j) of week.days"
+          :key="j"
+        >
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import Loading from '@/components/Loading'
 
 export default {
@@ -14,8 +26,13 @@ export default {
   },
   data () {
     return {
-      loading: true
+      loading: false
     }
+  },
+  computed: {
+    ...mapState({
+      history: state => state.commitHistory
+    })
   },
   mounted () {
     this.createCommitGraph()
@@ -28,9 +45,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #widget {
   position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100%;
   height: 100%;
   padding: .5rem;
@@ -38,5 +58,22 @@ export default {
   background-color: #fff;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, .3);
   box-sizing: border-box;
+  text-align: center;
+
+  .graph-wrap {
+
+    .week {
+      float: left;
+      max-width: 10px;
+
+      .day {
+        float: left;
+        width: 8px;
+        height: 8px;
+        background-color: green;
+        margin: 1px;
+      }
+    }
+  }
 }
 </style>
