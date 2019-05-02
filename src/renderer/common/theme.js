96dcaf74fd769a1b27d28b4f16562ca6178c47e4
leegeunhyeok <lghlove0509@naver.com>
2019-05-02 15:32:40 +0900

    ADD: Violet color theme

--- src/renderer/common/theme.js
+++ src/renderer/common/theme.js
@@ -2,7 +2,8 @@ const THEMES = [
   ['#ebedf0', '#c6e48b', '#7bc96f', '#239a3b', '#196127'],
   ['#ebedf0', '#addffd', '#56b4f5', '#257cf2', '#073777'],
   ['#ebedf0', '#f5a19e', '#ec625d', '#e7362f', '#561315'],
-  ['#ebedf0', '#f8db3a', '#ddb639', '#a58105', '#745a04']
+  ['#ebedf0', '#fae46b', '#f9b652', '#dd8808', '#935b05'],
+  ['#ebedf0', '#d7b7ff', '#b070ff', '#8624ff', '#600080']
 ]
 
 export default THEMES
