0fc750671c05475384ba204bad37ba1bb347b2c8
leegeunhyeok <lghlove0509@naver.com>
2019-05-02 15:25:27 +0900

    ADD: Color themes

--- src/renderer/common/theme.js
+++ src/renderer/common/theme.js
@@ -1,6 +1,8 @@
 const THEMES = [
   ['#ebedf0', '#c6e48b', '#7bc96f', '#239a3b', '#196127'],
-  ['#ebedf0', '#b7e9f7', '#4ab8f9', '#0d7aaf', '#264993']
+  ['#ebedf0', '#addffd', '#56b4f5', '#257cf2', '#073777'],
+  ['#ebedf0', '#f5a19e', '#ec625d', '#e7362f', '#561315'],
+  ['#ebedf0', '#f8db3a', '#ddb639', '#a58105', '#745a04']
 ]
 
 export default THEMES
