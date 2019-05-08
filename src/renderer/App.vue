f7f2455edf157a77adc91f22ed314d57dd2f84e4
leegeunhyeok <lghlove0509@naver.com>
2019-05-08 11:25:31 +0900

    UPDATE: Change default fonts

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -86,6 +86,15 @@ html, body, #app {
   height: 100%;
   margin: 0;
   padding: 0;
+  font-family: -apple-system
+                ,BlinkMacSystemFont
+                , 'Segoe UI'
+                , Helvetica
+                , Arial
+                , sans-serif
+                , 'Apple Color Emoji'
+                , 'Segoe UI Emoji'
+                , 'Segoe UI Symbol';
 }
 
 #app {
